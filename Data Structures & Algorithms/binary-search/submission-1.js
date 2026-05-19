56d44a272a3d7d69cf57bfb34c55a7ae66bd98e0
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-19 22:25:05 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     * 
+     * 
+     * target 4, expected 3
+     * [-1,0,2,4,6,8]
+     * 2
+     * 
+     */
+    search(nums, target) {
+
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (left <= right) {
+            const mid = Math.floor((right + left) / 2);
+            if(target === nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
