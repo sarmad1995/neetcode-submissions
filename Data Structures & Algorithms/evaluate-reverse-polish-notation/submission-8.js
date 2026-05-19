e1ead293889048e800ecab0d886688f531016dda
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-19 15:55:04 +0530

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     * 
+     * 1,2,3,4 + / 
+     */
+    evalRPN(tokens) {
+
+        const stack = [];
+        for(let i = 0; i<tokens.length; i ++) {
+            const char = tokens[i]
+            if(char === '-' || char === '+' || char === '/' || char === '*') {
+                const x = stack.pop();
+                const y = stack.pop();
+                const result = Math.trunc(this.operation(y, x, char));
+                console.log(result)
+                stack.push(result);
+            } else {
+                console.log(char)
+                stack.push(parseInt(char))
+            } 
+        }
+        return stack[stack.length -1]
+
+    }
+
+
+    operation(a, b, o) {
+        if(o === '+') {
+            return a + b;
+        } else if (o === '*') {
+            return a * b;
+        } else if (o === '-') {
+            return a - b;
+        } else if (o === '/') {
+            return a / b;
+        }
+    }
+}
