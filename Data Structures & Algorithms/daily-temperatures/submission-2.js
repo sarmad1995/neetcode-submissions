d3b736809fb337491df66e32d5e4cda07613cffe
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-19 16:17:51 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     *
+     * [0,0,0,0,0,0,0]
+     */
+    dailyTemperatures(temperatures) {
+        const result = new Array(temperatures.length).fill(0);
+        const stack = [[temperatures[0], 0]];
+
+        for(let i = 1; i<temperatures.length; i ++) {
+            const current = temperatures[i];
+            const top = stack[stack.length -1][0];
+            console.log(current)
+            if(current < top) {
+                stack.push([current, i])
+            } else {
+                while(stack[stack.length -1]?.[0] < current) {
+                    console.log(stack, current)
+                    const pop = stack.pop();
+                    result[pop[1]] = i - pop[1]
+                }
+            stack.push([current, i])            }
+        }
+        return result;
+
+    }
+}
