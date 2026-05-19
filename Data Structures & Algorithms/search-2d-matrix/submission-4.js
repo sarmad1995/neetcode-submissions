b0358173dd1030e27afc149f3cbb342ab4467c9b
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-19 16:51:53 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     * 
+     * 
+     * [1,2,3,4,5,6]
+     */
+    searchMatrix(matrix, target) {
+
+        const n = matrix[0].length -1 
+        for(let row = 0; row<matrix.length -1; row++) {
+            const left = matrix[row][0];
+            const right = matrix[row][n];
+            if(target <= right && target >= left) {
+               console.log("Found", row, target)
+                return this.binarySearch(matrix[row], target)
+            }
+        }
+        return false
+    }
+
+
+    binarySearch(arr, target) {
+        let left = 0;
+        let right = arr.length -1;
+        while(left <= right) {
+            const mid = Math.floor((right + left) /2);
+            if(target === arr[mid]) {
+                return true;
+            } else if(target > arr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false
+    }
+}
