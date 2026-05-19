3a959206d7064a98340f1bdfcbd2a25e18a71bd7
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-19 15:06:15 +0530

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        
+        let left = 0;
+        let right = s.length -1;
+
+        while(left < right) {
+            const l = s[left];
+            const r = s[right];
+
+            if(!isValid(l)) {
+                left ++;
+                continue;
+            } else if(!isValid(r)) {
+                right --;
+                continue;
+            }
+            if(l.toLowerCase() !== r.toLowerCase()) {
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return true;
+    }
+
+
+    isValid(char) {
+        return (char >= 'a' && char <= 'z') ||
+               (char >= 'A' && char <= 'Z') ||
+               (char >= '0' && char <= '9')
+    }
+}
