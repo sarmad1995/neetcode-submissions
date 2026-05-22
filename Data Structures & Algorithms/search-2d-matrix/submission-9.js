008da2fb7c637d2121291d904122aa6603f11382
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-22 15:33:07 +0530

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.js
@@ -0,0 +1,60 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const rowLength = matrix[0].length;
+        let top = 0
+        let bottom = matrix.length - 1;
+
+        console.log(top, bottom)
+
+        while(top <= bottom) {
+            console.log(top, bottom)
+            if(top === bottom) {
+                return this.binarySearch(matrix[top], target)
+            }
+
+            const mid = Math.floor((top + bottom) / 2);
+
+            console.log(mid)
+            const currentRowEnd = matrix[mid][rowLength - 1];
+            const currentRowStart = matrix[mid][0];
+
+            console.log(currentRowEnd, currentRowStart)
+            console.log(currentRowEnd >= target)
+            console.log(currentRowStart >= target )
+            if(currentRowEnd >= target && currentRowStart <= target) {
+                return this.binarySearch(matrix[mid],target)
+            } else if (currentRowEnd > target) {
+                bottom = mid -1;
+            } else {
+                top = mid + 1;
+            }
+
+        }
+        return false;
+
+    }
+
+    binarySearch(arr, target) {
+        console.log(arr, target)
+        let left = 0;
+        let right = arr.length -1;
+
+        while(left <= right) {
+            const mid = Math.floor((right + left ) / 2);
+            const current = arr[mid];
+            if(current === target) {
+                return true;
+            } else if (current < target) {
+                left = mid + 1;
+            } else {
+                right = mid -1;
+            }
+        }
+        return false;
+    }
+}
