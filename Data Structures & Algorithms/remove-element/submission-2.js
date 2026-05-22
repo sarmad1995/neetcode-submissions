4b24e6ea11cf6c0640d7a1dc961d66a5ab7915f3
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-23 00:38:26 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     * 
+     * Input: nums = [3,3,2,2,3], val = 3
+
+       Output: k = 2, nums = [2,2,_,_]
+     */
+    removeElement(nums, val) {
+        let insertionIndex = 0;
+
+        for(let i =0; i<nums.length; i++) {
+            if(nums[i] !== val) {
+                nums[insertionIndex++] = nums[i]
+            }
+        }
+        return insertionIndex
+    }
+}
