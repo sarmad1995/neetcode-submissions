ffebb4bb0533b8a6c66a52624ec35798eecd4f2a
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-24 22:45:49 +0530

    Add: binary-search-tree-iterator - submission-0

---
+++ Data Structures & Algorithms/binary-search-tree-iterator/submission-0.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    /**
+     * @constructor
+     * @param {TreeNode} root
+     */
+    constructor(root) {
+        this.root = root;
+        this.stack = [];
+        this.iterator = [];
+
+        let curr = root;
+        while(curr || this.stack.length >0) {
+            if(curr) {
+                this.stack.push(curr);
+                curr = curr.left;
+            } else {
+                const node = this.stack.pop();
+                this.iterator.unshift(node.val)
+                curr = node.right;
+            }
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    next() {
+        return this.iterator.pop();
+    }
+
+    /**
+     * @return {boolean}
+     */
+    hasNext() {
+        return this.iterator.length > 0;
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
