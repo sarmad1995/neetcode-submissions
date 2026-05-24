800b95c6edb35fe2f2789bd677d94fc6d7b51b3b
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-05-24 20:37:55 +0530

    Add: kth-smallest-integer-in-bst - submission-3

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-3.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} k
+     * @return {number}
+     */
+    kthSmallest(root, k) {
+        let count = 0;
+        const dfs = (root) => {
+            if(!root) {
+                return null;
+            }
+            dfs(root.left);
+            count++
+            if(count === k) {
+                return root.val;
+            }
+            dfs(root.right);
+        }
+        return dfs(root)
+    }
+}
