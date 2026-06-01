4a78391e2359c71281892d4de8da89d11a0edf2a
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-01 17:54:06 +0530

    Add: level-order-traversal-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-3.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[][]}
+     */
+    levelOrder(root) {
+        if(!root) {
+            return []
+        }
+        const result = [];
+        const queue = [root];
+        while(queue.length > 0) {
+            const l = queue.length;
+            const level = [];
+            for(let i = 0; i<l; i++) {
+                const node = queue.shift();
+                if(node.left) {
+                    queue.push(node.left);
+                }
+                if(node.right) {
+                    queue.push(node.right);
+                }
+                level.push(node.val)
+            }
+            result.push(level)
+        }
+        return result;
+    }
+}
