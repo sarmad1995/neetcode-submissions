d7ee0edae5546f3b6f5fc43b86ab5282c1ac33ec
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-04 23:31:26 +0530

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let xor = 0;
+
+        for(const n of nums) {
+            xor = xor ^ n
+        }
+        return xor;
+    }
+}
