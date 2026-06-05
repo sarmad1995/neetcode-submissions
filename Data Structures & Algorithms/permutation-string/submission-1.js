c155863281af87040fc3c49b0c07343c6c1c13c8
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-05 20:16:53 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        if(s1.length > s2.length) {
+            return false;
+        }
+        const freq = new Array(26).fill(0);
+        for(const ch of s1) {
+            const code = ch.charCodeAt(0) - 97;
+            freq[code] = freq[code] + 1;
+        }
+        const k = s1.length;
+        
+        const freq2 = new Array(26).fill(0);
+
+        for(let i = 0; i<k; i++) {
+            const code = s2[i].charCodeAt(0) - 97;
+            freq2[code] = freq2[code] + 1;
+        }
+        if(this.areFreqEqual(freq, freq2)) {
+            return true;
+        }
+        for(let r = k; r<s2.length; r++) {
+            const idx = s2[r-k].charCodeAt(0) - 97;
+            freq2[idx] = freq2[idx] - 1;
+
+            const code = s2[r].charCodeAt(0) - 97;
+            freq2[code] = freq2[code] + 1;
+            if(this.areFreqEqual(freq, freq2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    areFreqEqual(freq1, freq2) {
+        return freq1.join('') === freq2.join('')
+    }
+}
