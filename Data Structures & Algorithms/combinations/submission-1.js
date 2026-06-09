decf5055ea78cb128416078869c1b1ddcebae6e6
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-09 15:56:18 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number} k
+     * @return {number[][]}
+     */
+    combine(n, k) {
+        const res = [];
+        this.dfs(1, n, k, res, [])
+        return res;
+    }
+
+    dfs(idx, n, k , res, comb) {
+        if(comb.length == k) {
+            res.push([...comb])
+            return 
+        }
+        for(let i = idx; i<=n; i++){
+            comb.push(i)
+            this.dfs(i + 1, n, k, res, comb)
+            comb.pop();
+        }
+    }
+}
