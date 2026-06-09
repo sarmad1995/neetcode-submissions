d955e9cecc98d35f39da2b090f6798fd730360f4
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-09 22:11:04 +0530

    Add: isomorphic-strings - submission-5

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-5.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isIsomorphic(s, t) {
+        const mapST = new Map();
+        const mapTS = new Map();
+        if (s.length != t.length) {
+            return false;
+        }
+
+        for (let i = 0; i < s.length; i++) {
+            const c1 = s[i];
+            const c2 = t[i]
+            if (
+                (mapST.has(c1) && mapST.get(c1) !== c2) ||
+                (mapTS.has(c2) && mapTS.get(c2) !== c1)
+            ) {
+                return false;
+            }
+
+            mapST.set(c1, c2);
+            mapTS.set(c2, c1)
+        }
+        return true;
+    }
+}
