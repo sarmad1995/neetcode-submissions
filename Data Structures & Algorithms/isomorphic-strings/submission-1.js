43dc813a424db2a5b1f09db4e97fdc3537ab5415
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-09 22:07:53 +0530

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isIsomorphic(s, t) {
+        const map1 = {};
+        const map2 = {};
+        if(s.length != t.length) {
+            return false;
+        }
+
+        for(let i = 0; i<s.length; i++) {
+            if(map1[s[i]] && map1[s[i]] !== t[i]) return false;
+            if(map2[t[i]] && map2[t[i]] !== s[i]) return false;
+
+            map1[s[i]] = t[i]
+            map2[t[i]] = s[i]
+        }
+        return true;
+    }
+}
