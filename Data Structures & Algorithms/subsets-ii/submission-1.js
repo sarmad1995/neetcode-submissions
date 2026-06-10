0928ad8a46788032573d32f8c9ef032c5732ffc6
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-10 18:42:21 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsetsWithDup(arr) {
+        const res = [];
+        arr.sort();
+        this.backtrack([], arr, 0, res);
+        return res;
+    }
+
+    backtrack(path, arr, idx, res) {
+        res.push([...path])
+        for(let i = idx; i<arr.length; i++) {
+            if(i > idx && arr[i - 1] === arr[i]) {
+                path.push(arr[i]);
+                this.backtrack(path, arr, i + 1, res)
+                path.pop();
+            }
+        }
+    }
+}
