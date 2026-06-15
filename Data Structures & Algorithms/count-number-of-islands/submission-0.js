8a31dfa8b0fa188cb4491203c0901fd27e75b5c7
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-15 16:34:52 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        const n = grid.length;
+        const m = grid[0].length;
+        const dfs = (grid, r, c, visited) => {
+            if(Math.min(r, c) < 0 || r === n || c === m) {
+                return;
+            }
+            if(grid[r][c] === '0' || visited[r][c]) {
+                return;
+            }
+
+            visited[r][c] = true;
+
+            dfs(grid, r + 1, c, visited)
+            dfs(grid, r - 1, c, visited)
+            dfs(grid, r, c + 1, visited)
+            dfs(grid, r, c - 1, visited)
+
+        }   
+
+        const visited = [];
+
+        for(let i = 0; i<n; i++) {
+            visited[i] = [];
+            for(let j = 0; j<m; j++) {
+                visited[i][j] = false;
+            }
+        }
+        
+        let res = 0;
+        for(let i = 0; i<n; i++) {
+           for(let j = 0; j<m; j++) {
+                if(grid[i][j] === '1') {
+                    if(!visited[i][j]) {
+                        res++;
+                        dfs(grid, i, j, visited);
+                    }
+                }
+            } 
+        }
+        return res;
+    }
+}
