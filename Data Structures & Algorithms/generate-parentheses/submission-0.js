9b91572e52a43da32b8463bcb68f12897df154dd
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-06-16 23:10:02 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n) {
+        const res = [];
+        this.backtrack(n, 0, 0, [], res)
+        return res;
+    }
+
+    backtrack(n, open, close, curr, res,) {
+        console.log(curr)
+        if(open === n && close === n) {
+            res.push(curr.join(''))
+            return;
+        }
+        if(open < n) {
+            curr.push('(')
+            this.backtrack(n, open + 1, close, curr, res)
+            curr.pop();
+        } 
+        if(close < open) {
+            curr.push(')')
+            this.backtrack(n, open, close + 1, curr, res)
+            curr.pop()
+        }
+    }
+}
