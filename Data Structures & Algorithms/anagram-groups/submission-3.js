61f8502d61c6cdf02fe9c12f2b091d1b73138cf4
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-07-03 19:15:29 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+      const freq = {}
+      for(const word of strs) {
+        const arr = new Array(26).fill(0);
+
+        for(const ch of word) {
+          const code = ch.charCodeAt(0) - 97;
+          arr[code]++;
+        }
+        const key = arr.join(',');
+        if(freq[key] !== undefined) {
+          freq[key].push(word)
+        } else {
+          freq[key] = [word]
+        }
+      }
+      return Object.values(freq)
+    }
+}
