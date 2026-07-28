cbdc62af526d9bcb12f6db0bf1f1473ab88a156f
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-07-28 21:50:27 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if(nums.length === 1) {
+            return nums[0]
+        }
+        const dp = [nums[0], Math.max(nums[1], nums[0])]
+        
+        for(let i = 2; i< nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+        }
+        return dp[dp.length - 1]
+    }
+}
