e7ec7f09016aa4d7e50942f2dfc83149a5545405
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-08-18 10:23:29 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+        const freqS = new Map();
+        const freqT = new Map();
+        for (const ch of s) {
+            freqS.set(ch, (freqS.get(ch) || 0) + 1)
+        }
+        for (const ch of t) {
+            freqT.set(ch, (freqT.get(ch) || 0) + 1)
+        }
+
+        for (const [key, value] of freqS.entries()) {
+            console.log({ key, value })
+            console.log(freqT.has(key))
+            if (!freqT.has(key) || freqT.get(key) !== value) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
