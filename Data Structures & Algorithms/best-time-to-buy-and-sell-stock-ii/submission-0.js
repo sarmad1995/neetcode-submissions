0cb3869bff0e4fb8a18b3ffb1c358522328d073d
Sarmad <38377482+sarmad1995@users.noreply.github.com>
2026-08-23 15:22:02 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+
+        let totalProfit = 0;
+        for(let i = 0; i<prices.length - 1; i ++) {
+            const profit = prices[i + 1] -prices[i] 
+            console.log(profit)
+            if(profit > 0) {
+                totalProfit += profit;
+            }
+        }
+        return totalProfit;
+    }
+}
